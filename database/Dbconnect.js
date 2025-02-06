b5c3bf997e8450588ae5a817ffa94ff9f249e8f9
Deepank-01 <deepanksingh01@gmail.com>
2025-02-06 18:40:35 +0530

    V1 for backend

--- database/Dbconnect.js
+++ database/Dbconnect.js
@@ -1,10 +1,8 @@
 const mongoose=require("mongoose")
 require("dotenv").config()
 const dbconnect=()=>{
-    mongoose.connect(process.env.MONGODB_URL,{
-          useNewUrlParser:true,
-        useUnifiedTopology:true,
-    }).then(()=>{console.log("Db connected Suucessfully ")})
+    console.log(process.env.MONGODB_URL)
+    mongoose.connect(process.env.MONGODB_URL).then(()=>{console.log("Db connected Suucessfully ")})
     .catch((err)=>{console.log("Error in the conncetion with the DB  , error :",err)
         process.exit(1);
     })
--- routes/Message.js
+++ routes/Message.js
@@ -6,4 +6,4 @@ const {getsidebar,getmessage,sendmessage}=require("../controllers/Message")
 router.get("/user",protect,getsidebar)
 router.get("/get/:id",protect,getmessage)
 router.post("/send/:id",protect,sendmessage)
-module.exports=router
+module.exports=router  
